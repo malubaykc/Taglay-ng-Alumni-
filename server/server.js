dcc3491538c6ed1b4fa073e7c610ef0810943ec6
malubaykc <126657122+malubaykc@users.noreply.github.com>
2026-01-02 18:02:20 +0800

    fix

--- server/server.js
+++ server/server.js
@@ -22,4 +22,8 @@ mongoose
   .catch((err) => console.error(err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
